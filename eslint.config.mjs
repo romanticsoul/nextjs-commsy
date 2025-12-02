919eb8eb7b18df2c7ec34463868890637675bcd1
S5HX <yarulinainur@gmail.com>
2025-12-03 02:37:19 +0700

    updated ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -212,19 +212,3 @@ export default defineConfig([
 	...reactCompilerConfig,
 	prettier,
 ])
-
-// import { defineConfig, globalIgnores } from "eslint/config"
-// import nextVitals from "eslint-config-next/core-web-vitals"
-// import nextTs from "eslint-config-next/typescript"
-// import prettier from "eslint-config-prettier/flat"
-// import pluginImport from "eslint-plugin-import"
-
-// const eslintConfig = defineConfig([
-// 	pluginImport.flatConfigs.recommended,
-// 	...nextVitals,
-// 	...nextTs,
-// 	prettier,
-// 	globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-// ])
-
-// export default eslintConfig
